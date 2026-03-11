4ed953d5b57a05e53f746cc48dd65312f232aa94
Nicholas Will <cinderandcrown@gmail.com>
2026-03-11 19:26:47 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
   }, []);
 
   return (
-    <div className="dark app-shell-bg min-h-screen text-white">
+    <div className="dark app-shell-bg min-h-screen text-white" style={{ background: '#0A1628', color: '#E2E8F0' }}>
       <div className="relative z-10">{children}</div>
     </div>
   );
